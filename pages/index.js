e1e97dbda4b41d83ddd6a25f96bd017c80cbeb33
Karen <mooshi21824@gmail.com>
2020-12-06 21:31:27 +0800

    try tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+
 
 import Layout from '../components/layout'
 
@@ -12,11 +10,23 @@ export default function Home() {
 
   return (
     <Layout>
-      <main className={styles.main}>
-        <h3 className={styles.title}>
-          this is home page
-        </h3>
-      </main>
+  
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
+      <h3>
+        this is home page
+      </h3>
+      <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+        Click me
+      </button>
+  
     </Layout>
     
   )
